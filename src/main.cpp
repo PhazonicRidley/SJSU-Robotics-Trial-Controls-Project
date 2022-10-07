7fde8141d68fca1f5f7d4850e5881bb3004099af
PhazonicRidley <ma13hew@gmail.com>
2022-10-07 01:22:33 -0700

    added positive modulo operator to properly loop around a divisor

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 Servo myservo;
 int serv_value = 0;
 int raw_value;
+int positive_modulo(int number, int divisor);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -21,10 +23,18 @@ void loop() {
     raw_value = Serial.readString().toInt();
     Serial.print("Got: ");
     Serial.println(raw_value);
-    serv_value = raw_value % 181;
+    if (raw_value != 180)
+      serv_value = positive_modulo(raw_value, 180);
+    else
+      serv_value = raw_value;
     //serv_value = map(raw_value, 0, 1023, 0, 180);
     myservo.write(serv_value);
     delay(15);
   }
 
+}
+
+int positive_modulo(int number, int divisor)
+{
+  return (number % divisor + divisor) % divisor;
 }
