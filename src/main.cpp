91eb4858aa1b8102dfc67a24fe9dd0dc02ccc82a
PhazonicRidley <ma13hew@gmail.com>
2022-10-11 23:56:17 -0700

    added printed z axis acceleration data

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ Servo my_servo;
 double initial_position = 90; // initial value, where "up" is defined as.
 double servo_value = initial_position;
 const double LBS_to_g_constant = 8192; // using mode 1 for +-4gs for acceleration
-const double LSB_to_deg_constant = 131; // using mode 0 for +- 250 deg/sec for gyroscope
 bool running = false;
 
 // takes the positive remainder of a number given a divisor, intended to be used to "loop around" a number.
@@ -53,9 +52,9 @@ void set_starting_offset(String s)
 }
 
 void loop() {
-  int16_t raw_acc_x, raw_acc_y;
+  int16_t raw_acc_x, raw_acc_y, raw_acc_z;
   String cmd = "";
-  double acc_x, acc_y;
+  double acc_x, acc_y, acc_z;
 
   // read inputs
   if (Serial.available() > 0)
@@ -88,16 +87,19 @@ void loop() {
     Wire.beginTransmission(MPU);
     Wire.write(0x3B);
     Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 4, true); // read 4 bytes of data for x and y
+    Wire.requestFrom(MPU, 6, true); // read 4 bytes of data for x and y
   }
   while (Wire.available() == 0);
   raw_acc_x = Wire.read() << 8 | Wire.read(); // get x value from two registers
   raw_acc_y = Wire.read() << 8 | Wire.read(); // get y values from two registers
+  raw_acc_z = Wire.read() << 8 | Wire.read(); // get z values from two registers
   
   // calculate acceleration in x and y directions in g
   acc_x = (double)raw_acc_x / LBS_to_g_constant;
-  acc_y = (double)raw_acc_y / LBS_to_g_constant;  
-  Serial.println("Acceleration Force: X = " + String(acc_x) + " | Y = " + String(acc_y));
+  acc_y = (double)raw_acc_y / LBS_to_g_constant; 
+  acc_z = (double)raw_acc_z / LBS_to_g_constant; 
+  Serial.print("Acceleration Force: X = " + String(acc_x) + " | Y = " + String(acc_y));
+  Serial.println(" | Z = " + String(acc_z));
 
   // calculate positional angle with respect to the x and y vectors of acceleration
   double positional_angle = rad_to_deg(atan(acc_y / acc_x));
